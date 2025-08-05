9338644a30f73924ec3d3535035a19d9298ed481
API65I <amolnart@gmail.com>
2025-08-05 21:14:35 +0200

    Start py

---
+++ start_project.py
@@ -0,0 +1,15 @@
+# start_project.py - Indítófájl
+
+# Szükséges modulok importálása
+import os         # Operációs rendszerrel kapcsolatos műveletekhez (fájlok elérési útjának kezelése)
+import subprocess # Külső parancsok futtatása Pythonból (Streamlit alkalmazás indítása)
+import sys        # Rendszerspecifikus információk és műveletek (Python futtatókörnyezet elérési útjának lekérése)
+
+# A fájl dinamikus elérési útjának meghatározása
+script_dir = os.path.dirname(os.path.abspath(__file__))  # Megadja az aktuális fájl könyvtárát
+project_path = os.path.join(script_dir, 'project', 'project.py')  # Az elérési útvonalat a "script_dir" alapján határozza meg függetlenül attól honnan indítod a programot
+
+# A Streamlit alkalmazás indítása
+print("Elindítjuk a Streamlit alkalmazást...")
+subprocess.run([sys.executable, "-m", "streamlit", "run", project_path])
+
